113e6c6df674212d9f90d6bf4f3a5ed1209d008d
Enry6tz <enrique6tz@gmail.com>
2026-05-27 16:25:55 -0300

    bug/prisma config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
-  outputFileTracingIncludes: {
-    "/**/*": [
-      "./src/generated/prisma/**/*",
-      "./node_modules/@prisma/engines/**/*",
-    ],
-    "/api/**/*": [
-      "./src/generated/prisma/**/*",
-      "./node_modules/@prisma/engines/**/*",
-    ],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
